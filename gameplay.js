3f78a1e9f7b4c15d31e1d1e46d6e878a2dba01b8
rebecca <fulkerson.rebecca@gmail.com>
2022-10-04 11:52:49 -0400

    styled the logo page

--- gameplay.js
+++ gameplay.js
@@ -1,6 +1,7 @@
 console.log('Hey whats up hello')
-const truthButton = document.querySelector('#truth')
-const dareButton = document.querySelector('#dare')
+const truthBtn = document.getElementById('truthButton')
+
+const dareBtn = document.getElementById('dareButton')
 
 const truthQuestions = [
   "What's the last lie you told?",
@@ -17,57 +18,53 @@ const truthQuestions = [
 ]
 
 function truth() {
-  let newDiv = document.createElement('div')
   let truthH1 = document.createElement('h1')
 
   function getTruth() {
     let randomTruth = Math.floor(Math.random() * truthQuestions.length)
-    return truthQuestions[randomTruth]
+    return (truthQuestions[randomTruth] = truthH1.innerText)
   }
   getTruth()
 
-  truthH1.innerText
-
-  truthH1.appendChild(newDiv)
-  document.querySelector('.todOptions').appendChild(newDiv)
+  document.getElementById('questionSpot').appendChild(truthH1)
 }
 
 // function dare() {
 // }
 
-truthButton.addEventListener('click', truth())
+truthBtn.addEventListener('click', truth)
 // //dareButton.addEventListener('click', dare())
 
-class Player {
-  constructor(name) {
-    this.name = name
-    this.points = 0
-    this.truths = [
-      "What's the last lie you told?",
-      "What was the most embarrassing thing you've ever done on a date?",
-      'Have you ever accidentally hit something (or someone!) with your car?',
-      "Name someone you've pretended to like but actually couldn't stand.",
-      "What's your most bizarre nickname?",
-      "What's been your most physically painful experience?",
-      'What bridges are you glad that you burned?',
-      "What's the craziest thing you've done on public transportation?",
-      'If you met a genie, what would your three wishes be?',
-      'If you could write anyone on Earth in for President of the United States, who would it be and why?',
-      "What's the meanest thing you've ever said to someone else?"
-    ]
-    this.dares = []
-  }
-  getTruth() {
-    let randomTruth = Math.floor(Math.random() * this.truths.length)
-    return this.truths[randomTruth]
-  }
-  givePointsTruth() {
-    if ((completed = true)) {
-      //add 1 point to total
-    } else if ((completed = false)) {
-      //do not add point to player total
-    }
-  }
-}
+// class Player {
+//   constructor(name) {
+//     this.name = name
+//     this.points = 0
+//     this.truths = [
+//       "What's the last lie you told?",
+//       "What was the most embarrassing thing you've ever done on a date?",
+//       'Have you ever accidentally hit something (or someone!) with your car?',
+//       "Name someone you've pretended to like but actually couldn't stand.",
+//       "What's your most bizarre nickname?",
+//       "What's been your most physically painful experience?",
+//       'What bridges are you glad that you burned?',
+//       "What's the craziest thing you've done on public transportation?",
+//       'If you met a genie, what would your three wishes be?',
+//       'If you could write anyone on Earth in for President of the United States, who would it be and why?',
+//       "What's the meanest thing you've ever said to someone else?"
+//     ]
+//     this.dares = []
+//   }
+//   getTruth() {
+//     let randomTruth = Math.floor(Math.random() * this.truths.length)
+//     return this.truths[randomTruth]
+//   }
+//   givePointsTruth() {
+//     if ((completed = true)) {
+//       //add 1 point to total
+//     } else if ((completed = false)) {
+//       //do not add point to player total
+//     }
+//   }
+// }
 // const becca = new Player('Becca')
 // console.log(becca.getTruth())
